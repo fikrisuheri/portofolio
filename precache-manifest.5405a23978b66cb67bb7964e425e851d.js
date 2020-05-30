5eae94a1b9c05937fba7c33a93e38d350689f1de
fikrisuheri <fikrisuheri16@gmail.com>
2020-05-30 21:20:25 +0700

    Updates

---
+++ precache-manifest.5405a23978b66cb67bb7964e425e851d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd43f33408bc78ca8d6067ad8a17dd17",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "13c1be3aec5d36a4ed2e",
+    "url": "/portofolio/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "b6947e42b07070b722ef",
+    "url": "/portofolio/static/css/main.77d40fe3.chunk.css"
+  },
+  {
+    "revision": "13c1be3aec5d36a4ed2e",
+    "url": "/portofolio/static/js/2.22855325.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portofolio/static/js/2.22855325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6947e42b07070b722ef",
+    "url": "/portofolio/static/js/main.6b8171d7.chunk.js"
+  },
+  {
+    "revision": "19d1097e3554bac1aab3",
+    "url": "/portofolio/static/js/runtime-main.0168f7f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3271b64bf0a5c25ea55e2c6b1eaef39",
+    "url": "/portofolio/static/media/logo.d3271b64.png"
+  }
+]);
